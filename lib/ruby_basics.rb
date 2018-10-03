6c705cb8da4515ac8df7ae36ebcc886fb90250f3
Emily Asch <e.asch@me.com>
2018-10-03 16:53:20 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ return (num1/num2)
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
+  return value
 end
 
-def argue
-  
+def argue(argument)
+  return argument
 end
 
-def greeting
-
+def greeting(greeting, name)
+  
 end
 
 def return_a_value
-
+return_a_value("nice")
 end
 
 def last_evaluated_value
-
+last_evaluated_value("expert")
 end
 
 def pizza_party
-
+pizza_party("cheese")
 end
